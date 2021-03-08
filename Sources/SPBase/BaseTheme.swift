ecffa53750e590e96276a9d3f2f59d0e729cdad0
Gerald <geraldk675@gmail.com>
2021-03-08 16:54:28 +1100

    sdfas

--- Sources/SPBase/BaseTheme.swift
+++ Sources/SPBase/BaseTheme.swift
@@ -1,9 +1,9 @@
 import UIKit
 
 public struct BaseTheme {
-    let baseColor = UIColor.white
-    let textColor = UIColor.black
-    let highlightColor = UIColor.blue
-    let foreground = UIColor(named: "background", in: .module, compatibleWith: nil)
-    let background = UIColor(named: "foreground", in: .module, compatibleWith: nil)
+    public let baseColor = UIColor.white
+    public let textColor = UIColor.black
+    public let highlightColor = UIColor.blue
+    public let foreground = UIColor(named: "background", in: .module, compatibleWith: nil)
+    public let background = UIColor(named: "foreground", in: .module, compatibleWith: nil)
 }
