9fea6349d2b8af6fbb5a47022c495b816f0d94d3
Gerald <geraldk675@gmail.com>
2021-03-08 16:15:16 +1100

    Fix version

--- Sources/SPBase/BaseTheme.swift
+++ Sources/SPBase/BaseTheme.swift
@@ -4,6 +4,6 @@ struct BaseTheme {
     let baseColor = UIColor.white
     let textColor = UIColor.black
     let highlightColor = UIColor.blue
-    let foreground = UIColor(named: "background")
-    let background = UIColor(named: "foreground")
+    let foreground = UIColor(named: "background", in: .module, compatibleWith: nil)
+    let background = UIColor(named: "foreground", in: .module, compatibleWith: nil)
 }
