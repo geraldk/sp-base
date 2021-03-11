295ac2a640bc354b8656e20176e820b58483a17d
Gerald <geraldk675@gmail.com>
2021-03-11 18:52:47 +1100

    asdfasd

--- Sources/SPBase/BaseTheme.swift
+++ Sources/SPBase/BaseTheme.swift
@@ -2,7 +2,7 @@ import UIKit
 
 public class BaseTheme {
     public let baseColor = UIColor.white
-    public let textColor = UIColor.black
+    public let textColor = UIColor.blue
     public let highlightColor = UIColor.blue
     public let foreground = UIColor(named: "background", in: .module, compatibleWith: nil)
     public let background = UIColor(named: "foreground", in: .module, compatibleWith: nil)
